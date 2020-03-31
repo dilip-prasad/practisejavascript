d94bbad1b3aff7da9c42b90e16197fc751c6038f
dilip-prasad <dilipprasad934@gmail.com>
2020-03-31 17:28:26 +0530

    new_!

---
+++ practise javascript/bgimageslider.js
@@ -0,0 +1,31 @@
+
+  const pictures = [
+    "./pictures/photo1",
+    "./pictures/photo2",
+    "./pictures/photo3",
+    "./pictures/photo4",
+    "./pictures/photo5"
+  ];
+
+  const buttons = document.querySelectorAll('.btn')
+  const imageDIV = document.querySelector('.img-container')
+  let counter = 0
+
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      if (button.classList.contains('btn-left')){
+        counter--
+        if(counter < 0){
+          counter = pictures.length -1
+        }
+        imageDIV.style.backgroundImage = `url('${pictures[counter]}.jpg')`
+      }
+      if (button.classList.contains('btn-right')){
+        counter++
+        if(counter > pictures.length -1){
+          counter = 0
+        }
+        imageDIV.style.backgroundImage = `url('${pictures[counter]}.jpg')`
+      }
+    })
+  })
